26e51fbfcaf6180b71b4998b6602a312cf0cdde1
jbzmcs <jabezmacasero.cpe@gmail.com>
2025-04-11 14:23:03 +0800

    Fix: variable image size issue

--- data_module.py
+++ data_module.py
@@ -65,6 +65,7 @@ def get_dataloaders(case, config, filter_classes=None):
 
     # Base transform (without augmentation) for all splits.
     base_transform = T.Compose([
+        T.Resize((224,224)), # Fix: variable image size issue
         T.ToTensor(),
         T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
